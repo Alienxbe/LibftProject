7194f6e2a540572952283931ae64d80c4c0007d1
Alienxbe <martin.ykman@gmail.com>
2020-11-25 17:46:19 +0100

    Finishing functions

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mykman <mykman@student.19.be>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/18 12:29:50 by mykman            #+#    #+#             */
-/*   Updated: 2020/11/25 17:32:45 by mykman           ###   ########.fr       */
+/*   Created: 2020/11/25 17:33:51 by mykman            #+#    #+#             */
+/*   Updated: 2020/11/25 17:44:58 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*ptr_dest;
-	unsigned char	*ptr_src;
+	const unsigned char	*s;
+	unsigned char		*d;
 
-	if (!dest && !src)
-		return (0);
-	ptr_dest = (unsigned char *)dest;
-	ptr_src = (unsigned char *)src;
-	while (n--)
-		*(ptr_dest + n) = *(ptr_src + n);
-	return (dest);
+	s = (const unsigned char *)src;
+	if (dest < src)
+	{
+		d = dest;
+		while (++i <= len)
+			*d++ = *s++;
+	}
+	else
+		ft_memcpy(dst, src, len);
+	return (dest)
 }
